d14faf891f28eae3ea417c821d1c9b10bb6566b0
vlcpl <valentin.lecouple@gmail.com>
2018-10-08 21:13:22 +0200

    Update src/Utilitaires/Protocol.java

--- src/Utilitaires/Protocol.java
+++ src/Utilitaires/Protocol.java
@@ -8,7 +8,7 @@ public class Protocol {
 	public Protocol(String host, int port) {
 		// On essaye d'accéder au serveur
 		try {
-			ts = new Transport(host, port);
+			this.ts = new Transport(host, port);
 		} catch (Exception e) {
 			System.err.println("Unable to reach the server");
 			e.printStackTrace();
@@ -16,24 +16,32 @@ public class Protocol {
 		}
 	}
 
-	
+	/**
+	 * Sends a file path to a server
+	 * @param requestParameter
+	 * @throws ClassNotFoundException
+	 */
 	public void sendRequest(String requestParameter) throws ClassNotFoundException {
 		try {
-			ts.envoyer(requestParameter);
-			ts.fermer();
+			this.ts.envoyer(requestParameter);
 		} catch (IOException e) {
 			System.err.println("An error occured during the request, please retry later");
 		}
 	}
 	
+	/**
+	 * Receives a response from the server
+	 * @return
+	 * @throws Exception
+	 */
 	public Object receiveResponse() throws Exception {
-		String serverResponse = (String) ts.recevoir();
+		String serverResponse = (String) this.ts.recevoir();
 		if(serverResponse.equals("Good")) {
-			String fileContent = (String) ts.recevoir();
-			ts.fermer();
+			String fileContent = (String) this.ts.recevoir();
+			this.ts.fermer();
 			return fileContent;
 		} else {
-			ts.fermer();
+			this.ts.fermer();
 			throw new Exception ("The server did not send a good response");
 		}
 	}
